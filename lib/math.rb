1a179c776bbcb6f5c22512073abba88288a849a4
Christina Chianelli <cechianelli@gmail.com>
2020-05-20 01:58:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,29 +1,23 @@
 def addition(num1, num2)
- sum = num1+num2
- print sum
+
 end
 
 def subtraction(num1, num2)
- difference = num1-num2
- print difference
+
 end
 
 def division(num1, num2)
-  quotient = num1/num2
-  print quotient
+
 end
 
 def multiplication(num1, num2)
-  product = num1*num2
-  print product
+
 end
 
 def modulo(num1, num2)
-  remainder = num1%num2
-  print remainder
+
 end
 
 def square_root(num)
-  root = Math.sqrt(num)
-  print root
+
 end
